9db3d4a5d5c0622e7570274289df18a125b54c05
Bongani Nyoni <b.nyoni1101@gmail.com>
2023-12-17 10:48:57 +0200

    Initialising Lambda

--- lib/awscdk-cicd-pipeline-stack.ts
+++ lib/awscdk-cicd-pipeline-stack.ts
@@ -1,15 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { CodePipeline,  CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-//import { MyPipelineAppStage} from './stage'
+import { MyPipelineAppStage } from './stage';
 
 export class AwscdkCicdPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+   const pipeline =  new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('Falenyoni/awscdk-cicd-pipeline', 'main'), //
@@ -18,5 +17,23 @@ export class AwscdkCicdPipelineStack extends cdk.Stack {
                   'npx cdk synth']
       }),
     });
+
+    const testingStage  =  pipeline.addStage(new MyPipelineAppStage(this, 'test', {
+      env:{
+        account: '277819333518',
+        region: 'us-east-1'
+      }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage  =  pipeline.addStage(new MyPipelineAppStage(this, 'prod', {
+      env:{
+        account: '277819333518',
+        region: 'us-east-1'
+      }
+    }));
+
+
   }
 }
---
+++ lib/lambda/handler.ts
@@ -0,0 +1,9 @@
+
+
+export async function handler(event: string, context: string){
+    console.log('Stage Name is: ' + process.env.stage);
+    return {
+        body: 'Hello from a lambda Function',
+        statusCode: 200,
+    };
+}
